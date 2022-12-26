d8907ae06db6f6104c0705daf25aa62974e9383c
lakshmi12priya <121346748+lakshmi12priya@users.noreply.github.com>
2022-12-26 18:38:25 +0530

    Update exam_reg.php

--- exam_reg.php
+++ exam_reg.php
@@ -6,15 +6,21 @@
 <html>
     <head>
         <link rel="stylesheet" href="styles.css">
+        
     </head>
     <body>
     <?php
             include 'fetch_data.php';
-        ?>
+    ?>
+    
+    <div class='img'>
+        <img src="ptu_logo.png" alt="PTU logo" style=" width: 10%;height: 30%;display: block;margin-left: auto;margin-right: auto;">
+    </div>
     <div class='Header'>
     <h1>Puducherry Technological University<br>(PTU)</h1>
     </div>
     <div class='Details'>
+        <h2><b>Examination Wing</b></h2>
         <h3>To<br>
         <b>Name</b>,<br>
         Register Nr:<?php echo "$_SESSION[regno]" ?> <br>
@@ -27,13 +33,55 @@
     </h3>
     </div>
     <h1></h1>
-    <table class='table1'>
+    <table class='table1' style="width:100%">
         <tr>
-            <th>Subject Code</th>
+            <th style="width:100px">Subject Code</th>
             <th>Subject Name</th>
             <th>Type</th>
-            <th>Fees in INR</th>
+            <th  style="width:15%">Fees in INR</th>
         </tr>
+        <?php
+            $fee_sum = 0;  
+            foreach($subject as $x)
+            {
+                if(trim($x['course_type'])=='TY'){
+                    $x['course_type'] = 'Theory';
+                    $fees='250';
+                }
+                elseif(trim($x['course_type'])=='LB'){
+                    $x['course_type'] = 'Laboratory';
+                    $fees='350';
+                }
+                elseif(trim($x['course_type'])=='MC'){
+                    $x['course_type'] = 'Mandatory Course';
+                    $fees='0';
+                }
+                else{
+                    echo("else.....".$x['course_type']);
+                }
+
+                echo "
+                <tr>
+                    <td>".$x['course_code']."</td>
+                    <td>".$x['course_name']."</td>
+                    <td>".$x['course_type']."</td>
+                    <td>".$fees."</td>
+                </tr>";
+
+            $fee_sum += $fees;
+            } 
+        ?>
+       
+    </table>  
+    <table class='table2' style="width:100%">
+        <?php
+        $applnfees = '100';
+        $marksfees = '50';
+        $totalfees = $applnfees + $marksfees + $fee_sum;
+        ?>
+        <tr><td>Application Fees</td><td  style="width:15%"><?php echo($applnfees); ?></td></tr>
+        <tr><td>Statement of Marks</td><td  style="width:15%"><?php echo($marksfees); ?></td></tr>
+        <tr><td>Total Exam Fees Payable</td><td><?php echo($totalfees); ?></td></tr>   
     </table>
     </body>
 </html>
